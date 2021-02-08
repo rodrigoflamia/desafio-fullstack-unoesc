930db4d1c3cf2f68361bcaf5ddc9ecc40c1c9c18
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2021-02-08 10:19:53 -0300

    Spring Initializr.

---
+++ src/main/java/br/edu/unoesc/desafiofullstackunoesc/DesafioFullstackUnoescApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.desafiofullstackunoesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFullstackUnoescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFullstackUnoescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unoesc/desafiofullstackunoesc/DesafioFullstackUnoescApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.desafiofullstackunoesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFullstackUnoescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
